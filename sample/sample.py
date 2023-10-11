4cdc88e7554300fef17b22d8f510444caaec4b49
Tyler Carbone <tcarbone073@gmail.com>
2023-10-11 17:08:02 +0000

    Create sample/sample.py

---
+++ sample/sample.py
@@ -0,0 +1,29 @@
+class Foo:
+    """
+    This is a docstring.
+    """
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def do_something_cool(self):
+        try:
+            for i in [1, 2, 3]:
+                print(i)
+        except Exception:
+            # Oh no!
+            raise Exception(f"Something bad happened. i was {i}.")
+
+
+class Foo(Bar):
+    
+    def __init__(self, c, d):
+        super().__init__(a=1, b=2)
+        self.c = c
+        self.d = d
+
+    @classmethod
+    def whoami(cls) -> str:
+        """What, in heaven's name, is MY name??"""
+        return cls.__name__
